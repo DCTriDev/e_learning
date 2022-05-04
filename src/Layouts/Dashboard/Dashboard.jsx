649a8da3519ee1e51af8c1dd34996f29ac9f2f6e
DinhCongTri <dinhcongtri9597@gmail.com>
2022-05-04 22:49:54 +0700

    upgrade antd libs, change dashboard layout.

--- src/Layouts/Dashboard/Dashboard.jsx
+++ src/Layouts/Dashboard/Dashboard.jsx
@@ -1,49 +1,35 @@
 import React, {useState} from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import {Layout, Menu, Breadcrumb} from 'antd';
 import {
-    DesktopOutlined,
-    PieChartOutlined,
-    FileOutlined,
-    TeamOutlined,
-    UserOutlined,
+    DesktopOutlined, PieChartOutlined,
 } from '@ant-design/icons';
-const { Header, Content, Footer, Sider } = Layout;
+import './Dashboard.css';
+
+const {Header, Content, Sider} = Layout;
 
 function getItem(label, key, icon, children) {
     return {
-        key,
-        icon,
-        children,
-        label,
+        key, icon, children, label,
     };
 }
 
-const items = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
-        getItem('Tom', '3'),
-        getItem('Bill', '4'),
-        getItem('Alex', '5'),
-    ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
-];
+const items = [getItem('Quản Lý Khóa Học', '1', <PieChartOutlined/>), getItem('Quản Lý Người Dùng', '2',
+    <DesktopOutlined/>),];
 
-function Dashboard() {
+function Dashboard(Component) {
     const [collapsed, setCollapsed] = useState(false);
     const onCollapse = (collapsed) => {
         setCollapsed(collapsed);
     };
-    return (
-        <Layout
+    return (props) => {
+        return (<Layout
             style={{
                 minHeight: '100vh',
             }}
         >
             <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-                <div className="logo" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+                <div className="logo"/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
             </Sider>
             <Layout className="site-layout">
                 <Header
@@ -68,23 +54,15 @@ function Dashboard() {
                     <div
                         className="site-layout-background"
                         style={{
-                            padding: 24,
-                            minHeight: 360,
+                            padding: 24, minHeight: 360,
                         }}
                     >
-                        Bill is a cat.
+                        <Component {...props} />
                     </div>
                 </Content>
-                <Footer
-                    style={{
-                        textAlign: 'center',
-                    }}
-                >
-                    Ant Design ©2018 Created by Ant UED
-                </Footer>
             </Layout>
-        </Layout>
-    );
+        </Layout>);
+    }
 }
 
 export default Dashboard;
