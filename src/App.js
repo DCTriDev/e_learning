d591fdc3e42a81a2d0ef755f4fa6c64d8bc8540d
stew15th <huymanucianlxag@gmail.com>
2022-05-06 01:32:59 +0700

    event-about

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./Pages/Home";
 import CourseCatalog from "./Pages/CourseCatalog";
 import LoadingAnim from "./Components/LoadingAnim";
 import CourseManagement from "./Pages/CourseManagement";
 import Search from "./Pages/Search";
+import About from "./Pages/About/About";
+import Events from "./Pages/Events/Events";
 
 function App() {
     return (
         <div>
-            <LoadingAnim/>
+            <LoadingAnim />
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/" component={HomePage}/>
-                    <Route exact path="/" component={HomePage}/>
-                    <Route exact path='/course-catalog/:maDanhMuc' render={({match}) => {
-                        return <CourseCatalog maDanhMuc={match.params.maDanhMuc}/>
-                    }}/>
-                    <Route exact path='/search/:input' component={Search} />
-                    <Route exact path='/dashboard/course-manage' render={({match}) => {
-                        return <CourseManagement {...match}/>
-                    }}/>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/" component={HomePage} />
+                    <Route
+                        exact
+                        path="/course-catalog/:maDanhMuc"
+                        render={({ match }) => {
+                            return (
+                                <CourseCatalog
+                                    maDanhMuc={match.params.maDanhMuc}
+                                />
+                            );
+                        }}
+                    />
+                    <Route exact path="/search/:input" component={Search} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/events" component={Events} />
+
+                    <Route
+                        exact
+                        path="/dashboard/course-manage"
+                        render={({ match }) => {
+                            return <CourseManagement {...match} />;
+                        }}
+                    />
                 </Switch>
             </BrowserRouter>
         </div>
