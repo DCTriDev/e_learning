6be7961dd5a3708a081d89b0b85f779a42b40627
anh <quanganhleee98@gmail.com>
2022-05-08 10:45:22 +0700

    'css'

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import LogInForm from "./LogInForm/LogInForm";
 
 export default function Login() {
-    return (
-        <div className=" flex justify-center items-center py-20">
-            <img
-                src="https://pixerio.com/wp-content/uploads/2020/11/E-learning-Company.png"
-                width={500}
-            />
-            <div className="">
-                <LogInForm />
-            </div>
-        </div>
-    );
+  return (
+    <div className=" flex flex-col lg:flex-row justify-between items-center py-20">
+      <div className="w-full lg:w-1/2  flex justify-center lg:px-14 px-5">
+        <img
+          className="w-full "
+          src="https://pixerio.com/wp-content/uploads/2020/11/E-learning-Company.png"
+          alt=""
+        />
+      </div>
+      <div className="w-full lg:w-1/2 lg:px-14 px-5 ">
+        <LogInForm />
+      </div>
+    </div>
+  );
 }
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import SignUpForm from "./SignUpForm/SignUpForm";
 
 export default function SignUp() {
-    return (
-        <div className=" flex justify-center items-center py-20">
-            <img
-                src="https://pixerio.com/wp-content/uploads/2020/11/E-learning-Company.png"
-                width={500}
-            />
-            <div className="">
-                <SignUpForm />
-            </div>
-        </div>
-    );
+  return (
+    <div className=" flex justify-center items-center py-20 lg:flex-row flex-col">
+      <div className=" w-full lg:w-1/2 flex  justify-center lg:px-14">
+        <img
+          className="w-full"
+          src="https://pixerio.com/wp-content/uploads/2020/11/E-learning-Company.png"
+          alt=""
+        />
+      </div>
+      <div className="w-full lg:w-1/2 flex justify-center">
+        <SignUpForm />
+      </div>
+    </div>
+  );
 }
