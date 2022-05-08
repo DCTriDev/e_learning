f7e9bcbbad2d628903a398cc14853749f5fa4586
anh <quanganhleee98@gmail.com>
2022-05-08 20:58:07 +0700

    'css-course-detail'

--- src/Pages/CourseDetail/index.jsx
+++ src/Pages/CourseDetail/index.jsx
@@ -1,44 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import Layout from "../../Layouts";
 import httpService from "../../Services/http.service";
-import {useParams} from "react-router-dom";
-
+import { useParams } from "react-router-dom";
+import "./index.css";
 function CourseDetail() {
-    let [courseDetail, setCourseDetail] = useState({});
-    let {courseID} = useParams();
-    console.log(courseID)
-    useEffect(() => {
-        httpService.getCourseDetail(courseID)
-            .then(res => {
-                setCourseDetail(res.data);
-            })
-    }, []);
+  let [courseDetail, setCourseDetail] = useState({});
+  console.log(courseDetail);
+  let { courseID } = useParams();
+  console.log(courseID);
+  useEffect(() => {
+    httpService.getCourseDetail(courseID).then((res) => {
+      setCourseDetail(res.data);
+    });
+  }, []);
 
-    const renderCourseDetail = () => {
-        return (
-            courseDetail ? (
-                <div className="lg:container px-12">
-                    <h2 className='text-center m-0'>{courseDetail.tenKhoaHoc}</h2>
-                    <div className="grid grid-cols-2  gap-3">
-                        <div className="col-span-1 self-center">
-                            <img className='w-full ' src={courseDetail.hinhAnh} alt=""/>
-                        </div>
-                        <div className="col-span-1">
-                            <h4 className='text-justify'>{courseDetail.moTa}</h4>
-                        </div>
-
-                    </div>
-                    <div className="">
-                        <p className='italic text-right mr-5 text-gray-600'>Lượt xem: {courseDetail.luotXem}</p>
-                    </div>
-                </div>) : null
-        )
-    };
-    return (
-        <div className='mt-20'>
-            {renderCourseDetail()}
+  const renderCourseDetail = () => {
+    return courseDetail ? (
+      <div className="flex flex-col ">
+        <img
+          className="course-img w-full md:object-fill h-[50vh] object-cover"
+          src={courseDetail.hinhAnh}
+          alt=""
+        />
+        <span className=" self-end px-12 ">
+          Ngày tạo : {courseDetail.ngayTao}{" "}
+        </span>
+        <div className="px-12">
+          <div className=" mt-12 space-y-10">
+            <h2 className="course-title text-center m-0 text-3xl font-bold uppercase font-sans ">
+              {courseDetail.tenKhoaHoc}
+            </h2>
+            <h4 className="course-desc text-justify text-gray-500 text-lg tracking-[1px] ">
+              {courseDetail.moTa}
+            </h4>
+          </div>
+          <p className="italic text-right mr-5 text-gray-600 text-lg">
+            Lượt xem: {courseDetail.luotXem}
+          </p>
         </div>
-    );
+      </div>
+    ) : null;
+  };
+  return <div className="mt-20">{renderCourseDetail()}</div>;
 }
 
 export default Layout(CourseDetail);
