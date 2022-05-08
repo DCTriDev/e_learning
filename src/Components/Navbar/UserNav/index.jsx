09db600b34816366561672725ba53beda138ab1b
anh <quanganhleee98@gmail.com>
2022-05-08 23:05:01 +0700

    'css'

--- src/Components/Navbar/UserNav/index.jsx
+++ src/Components/Navbar/UserNav/index.jsx
@@ -1,73 +1,83 @@
-import React from 'react';
-import {Dropdown, Menu} from 'antd';
-import {NavLink} from "react-router-dom";
+import React from "react";
+import { Dropdown, Menu } from "antd";
+import { NavLink } from "react-router-dom";
 import localServices from "../../../Services/localServices";
 
 const menu = (
-    <Menu className='rounded-xl p-2 translate-x-1/4'>
-        <Menu.Item key="0" className='hover:bg-blue-500 rounded-lg'>
-            <NavLink to="/profile" className='hover:text-white duration-300 transition-all ease-in-out'>
-                <span className='flex items-center space-x-2'>
-                    <ion-icon className='self-center' name="person-outline"/>
-                    <span>Profile</span>
-                </span>
-            </NavLink>
-        </Menu.Item>
-        <Menu.Item key="1" className='hover:bg-blue-500 rounded-lg'>
-            <NavLink to="/profile" className='hover:text-white duration-300 transition-all ease-in-out'>
-                <span className='flex items-center space-x-2'>
-                    <ion-icon name="settings-outline"/>
-                    <span>Settings</span>
-                </span>
-            </NavLink>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item key="2" className="hover:bg-blue-500 rounded-lg">
-            <NavLink
-                exact
-                to="/"
-                className="hover:text-white duration-300 transition-all ease-in-out">
-                <span className="flex items-center space-x-2">
-                    <ion-icon name="log-out-outline" />
-                    <span>Logout</span>
-                </span>
-            </NavLink>
-        </Menu.Item>
-    </Menu>
+  <Menu className="rounded-xl p-2 translate-x-1/4">
+    <Menu.Item key="0" className="hover:bg-blue-500 rounded-lg">
+      <NavLink
+        to="/profile"
+        className="hover:text-white duration-300 transition-all ease-in-out"
+      >
+        <span className="flex items-center space-x-2">
+          <ion-icon className="self-center" name="person-outline" />
+          <span>Profile</span>
+        </span>
+      </NavLink>
+    </Menu.Item>
+    <Menu.Item key="1" className="hover:bg-blue-500 rounded-lg">
+      <NavLink
+        to="/profile"
+        className="hover:text-white duration-300 transition-all ease-in-out"
+      >
+        <span className="flex items-center space-x-2">
+          <ion-icon name="settings-outline" />
+          <span>Settings</span>
+        </span>
+      </NavLink>
+    </Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="2" className="hover:bg-blue-500 rounded-lg">
+      <NavLink
+        onClick={localServices.removeUserInfo}
+        exact
+        to="/"
+        className="hover:text-white duration-300 transition-all ease-in-out"
+      >
+        <span className="flex items-center space-x-2">
+          <ion-icon name="log-out-outline" />
+          <span>Logout</span>
+        </span>
+      </NavLink>
+    </Menu.Item>
+  </Menu>
 );
 
 function UserNav() {
-    let userInfo = localServices.getUserInfo();
-    console.log(userInfo);
-    return userInfo?.accessToken ? (
-        <div className="flex items-center justify-center space-x-2">
-            <div>Xin chào, <span className='text-red-600'>{userInfo?.hoTen}</span></div>
-            <Dropdown overlay={menu} trigger={["click"]}>
-                <a
-                    className="ant-dropdown-link"
-                    onClick={(e) => e.preventDefault()}>
-                    <img
-                        className="w-12 h-12 object-cover rounded-full"
-                        src="https://picsum.photos/200/200"
-                        alt=""
-                    />
-                </a>
-            </Dropdown>
-        </div>
-    ) : (
-        <div>
-            <NavLink
-                to="/login"
-                className="bg-green-500 px-3 py-2 hover:scale-105 hover:bg-green-700 text-white duration-300 transition-all rounded-xl">
-                Log In
-            </NavLink>
-            <NavLink
-                to="/signup"
-                className="bg-red-500 mx-3 px-3 py-2 hover:scale-105 hover:bg-red-700 text-white duration-300 transition-all rounded-xl">
-                Sign Up
-            </NavLink>
-        </div>
-    );
+  let userInfo = localServices.getUserInfo();
+  console.log(userInfo);
+  return userInfo?.accessToken ? (
+    <div className="flex items-center justify-center space-x-2">
+      <div>
+        Xin chào, <span className="text-red-600">{userInfo?.hoTen}</span>
+      </div>
+      <Dropdown overlay={menu} trigger={["click"]}>
+        <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+          <img
+            className="w-12 h-12 object-cover rounded-full"
+            src="https://picsum.photos/200/200"
+            alt=""
+          />
+        </a>
+      </Dropdown>
+    </div>
+  ) : (
+    <div>
+      <NavLink
+        to="/login"
+        className="bg-green-500 px-3 py-2 hover:scale-105 hover:bg-green-700 text-white duration-300 transition-all rounded-xl"
+      >
+        Log In
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className="bg-red-500 mx-3 px-3 py-2 hover:scale-105 hover:bg-red-700 text-white duration-300 transition-all rounded-xl"
+      >
+        Sign Up
+      </NavLink>
+    </div>
+  );
 }
 
 export default UserNav;
--- src/Pages/LogIn/LogInForm/LogInForm.jsx
+++ src/Pages/LogIn/LogInForm/LogInForm.jsx
@@ -5,85 +5,91 @@ import httpService from "../../../Services/http.service";
 import localServices from "../../../Services/localServices";
 
 export default function LogInForm() {
-    let history = useHistory();
+  let history = useHistory();
 
-    const handleLogin = (values) => {
-        return httpService
-            .login(values)
-            .then((res) => {
-                console.log(res);
-                localServices.setUserInfo(res.data)
-                history.push("/");
-            })
-            .catch(({ err }) => {
-                message.error(err.response.data);
-            });
-    };
+  const handleLogin = (values) => {
+    return httpService
+      .login(values)
+      .then((res) => {
+        console.log(res);
+        message.success("Bạn đã đăng nhập thành công");
+        localServices.setUserInfo(res.data);
+        history.push("/");
+      })
+      .catch(({ err }) => {
+        message.error(err.response.data);
+      });
+  };
 
-    const onFinish = (values) => {
-        handleLogin(values);
-    };
+  const onFinish = (values) => {
+    handleLogin(values);
+  };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log("Failed:", errorInfo);
-    };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
-    return (
-        <Form
-            name="basic"
-            layout="vertical"
-            labelCol={{
-                span: 24,
-            }}
-            wrapperCol={{
-                span: 24,
-            }}
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off">
-            <Form.Item
-                label="Tài khoản"
-                name="taiKhoan"
-                rules={[
-                    {
-                        required: true,
-                        message: "Vui lòng nhập tài khoản!",
-                    },
-                    { whitespace: true },
-                ]}>
-                <Input />
-            </Form.Item>
+  return (
+    <Form
+      name="basic"
+      layout="vertical"
+      labelCol={{
+        span: 24,
+      }}
+      wrapperCol={{
+        span: 24,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Tài khoản"
+        name="taiKhoan"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập tài khoản!",
+          },
+          { whitespace: true },
+        ]}
+      >
+        <Input />
+      </Form.Item>
 
-            <Form.Item
-                label="Mật khẩu"
-                name="matKhau"
-                rules={[
-                    {
-                        required: true,
-                        message: "Vui lòng nhập mật khẩu!",
-                    },
-                    { whitespace: true },
-                    {
-                        min: 6,
-                        message: "Mật khẩu không thể ít hơn 6 ký tự",
-                    },
-                ]}>
-                <Input.Password />
-            </Form.Item>
+      <Form.Item
+        label="Mật khẩu"
+        name="matKhau"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập mật khẩu!",
+          },
+          { whitespace: true },
+          {
+            min: 6,
+            message: "Mật khẩu không thể ít hơn 6 ký tự",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
 
-            <Form.Item
-                wrapperCol={{
-                    span: 24,
-                }}>
-                <Button
-                    className=" bg-green-theme text-white border-none"
-                    htmlType="submit">
-                    Đăng nhập
-                </Button>
-            </Form.Item>
-        </Form>
-    );
+      <Form.Item
+        wrapperCol={{
+          span: 24,
+        }}
+      >
+        <Button
+          className=" bg-green-theme text-white border-none"
+          htmlType="submit"
+        >
+          Đăng nhập
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
