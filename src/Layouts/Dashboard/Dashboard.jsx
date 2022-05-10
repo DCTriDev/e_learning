fb2ab06af94db079ef87255aa2b359c1db6049a0
anh <quanganhleee98@gmail.com>
2022-05-10 13:08:53 +0700

    'fix-err-user-nav'

--- src/Layouts/Dashboard/Dashboard.jsx
+++ src/Layouts/Dashboard/Dashboard.jsx
@@ -1,65 +1,76 @@
-import React, {useState} from 'react';
-import {Layout, Menu, Breadcrumb} from 'antd';
-import {
-    DesktopOutlined, PieChartOutlined,
-} from '@ant-design/icons';
-import './Dashboard.css';
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+import { DesktopOutlined, PieChartOutlined } from "@ant-design/icons";
+import "./Dashboard.css";
 import CourseManagement from "../../Pages/CourseManagement";
 import UserManagement from "../../Pages/userManagement";
 
-const {Header, Content, Sider} = Layout;
+const { Header, Content, Sider } = Layout;
 
-function getItem(label, key, icon, children) {
-    return {
-        key, icon, children, label,
-    };
+function getItem(label, key, icon, items) {
+  return {
+    key,
+    icon,
+    items,
+    label,
+  };
 }
 
 const items = [
-    getItem('Quản Lý Khóa Học', '1', <PieChartOutlined/>),
-    getItem('Quản Lý Người Dùng', '2', <DesktopOutlined/>),
+  getItem("Quản Lý Khóa Học", "1", <PieChartOutlined />),
+  getItem("Quản Lý Người Dùng", "2", <DesktopOutlined />),
 ];
 
 function Dashboard(props) {
-    const [collapsed, setCollapsed] = useState(false);
-    const onCollapse = (collapsed) => {
-        setCollapsed(collapsed);
-    };
-    const [renderKey, updateRenderKey] = useState('1');
-    const handleRenderContent = key =>{
-        switch (key) {
-            case '1': return <CourseManagement/>
-            case '2': return <UserManagement/>
-        }
+  const [collapsed, setCollapsed] = useState(false);
+  const onCollapse = (collapsed) => {
+    setCollapsed(collapsed);
+  };
+  const [renderKey, updateRenderKey] = useState("1");
+  const handleRenderContent = (key) => {
+    switch (key) {
+      case "1":
+        return <CourseManagement />;
+      case "2":
+        return <UserManagement />;
     }
-    const handleMenuClick = menu => {
-        updateRenderKey(menu.key)
-    }
-    return (<Layout
-        style={{
-            minHeight: '100vh',
-        }}
+  };
+  const handleMenuClick = (menu) => {
+    updateRenderKey(menu.key);
+  };
+  return (
+    <Layout
+      style={{
+        minHeight: "100vh",
+      }}
     >
-        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse} >
-            <div className="logo"/>
-            <Menu theme="dark" defaultSelectedKeys={[renderKey]} mode="inline" items={items} onClick={handleMenuClick}/>
-        </Sider>
-        <Layout className="site-layout">
-            <Header
-                className="site-layout-background"
-                style={{
-                    padding: 0,
-                }}
-            />
-            <Content
-                style={{
-                    margin: '0 16px',
-                }}
-            >
-                {handleRenderContent(renderKey)}
-            </Content>
-        </Layout>
-    </Layout>);
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={[renderKey]}
+          mode="inline"
+          items={items}
+          onClick={handleMenuClick}
+        />
+      </Sider>
+      <Layout className="site-layout">
+        <Header
+          className="site-layout-background"
+          style={{
+            padding: 0,
+          }}
+        />
+        <Content
+          style={{
+            margin: "0 16px",
+          }}
+        >
+          {handleRenderContent(renderKey)}
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default Dashboard;
--- src/Pages/UserDetail/index.jsx
+++ src/Pages/UserDetail/index.jsx
@@ -1,121 +1,144 @@
-import React, {useEffect, useState} from "react";
-import {Form, Input, Tabs, message} from "antd";
-import {Modal} from 'antd'
+import React, { useEffect, useState } from "react";
+import { Form, Input, Tabs, message } from "antd";
+import { Modal } from "antd";
 import Layout from "../../Layouts";
-import {useDispatch, useSelector} from "react-redux";
-import {fetchUserDetail} from "../../Redux/Slice/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserDetail } from "../../Redux/Slice/userSlice";
 import CourseList from "./Course/CourseList";
 import Profile from "./Profile";
 import "./index.css";
-
-const {TabPane} = Tabs;
-
+const { TabPane } = Tabs;
 function UserDetail() {
-    const [visible, setVisible] = useState(false);
-    const [confirmLoading, setConfirmLoading] = useState(false);
-    const dispatch = useDispatch();
-    const userDetail = useSelector((state) => state.userSlice.userDetail);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const dispatch = useDispatch();
+  const userDetail = useSelector((state) => state.userSlice.userDetail);
 
-    const showModal = () => {
-        setVisible(true);
-    };
+  const showModal = () => {
+    setVisible(true);
+  };
 
-    useEffect(() => {
-        if (!userDetail) {
-            showModal()
-        }else{
-            hideModal()
-            message.success('Đăng Nhập Thành Công!')
-        }
-    }, [userDetail]);
-
-    const handleOk = () => {
-        setConfirmLoading(true);
-    };
-
-    const hideModal = () => {
-        if (userDetail) {
-            setVisible(false);
-            setConfirmLoading(false);
-        }
+  useEffect(() => {
+    if (!userDetail) {
+      showModal();
+    } else {
+      hideModal();
+      message.success("Đăng Nhập Thành Công!");
     }
+  }, [userDetail]);
 
+  const handleOk = () => {
+    setConfirmLoading(true);
+  };
 
-    return (
-        <div className=" mt-20">
-            <Modal
-                title={<span className='text-red-600 italic bold'>Vui lòng đăng nhập để tiếp tục!</span>}
-                visible={visible}
-                confirmLoading={confirmLoading}
-                closable={false}
-                okButtonProps={{htmlType: 'submit', className: 'btn', form: 'identity'}}
-                footer={[
-                    <button onClick={() => {
-                        window.location.href = '/'
-                    }} className='btn btn-red mr-3'>Thoát</button>,
-                    <button onClick={handleOk} form='identity' className='btn btn-green' type='submit'>Đăng Nhập</button>
-                ]}
+  const hideModal = () => {
+    if (userDetail) {
+      setVisible(false);
+      setConfirmLoading(false);
+    }
+  };
+
+  return (
+    <div className=" mt-20">
+      <Modal
+        title={
+          <span className="text-red-600 italic bold">
+            Vui lòng đăng nhập để tiếp tục!
+          </span>
+        }
+        visible={visible}
+        confirmLoading={confirmLoading}
+        closable={false}
+        okButtonProps={{
+          htmlType: "submit",
+          className: "btn",
+          form: "identity",
+        }}
+        footer={
+          <div>
+            <button
+              onClick={() => {
+                window.location.href = "/";
+              }}
+              className="btn btn-red mr-3"
             >
-                <Form
-                    name="identity"
-                    labelCol={{span: 24}}
-                    wrapperCol={{span: 24}}
-                    onFinish={(values) => {
-                        console.log(values);
-                        dispatch(fetchUserDetail(values));
-                    }}
-                    onFinishFailed={() => {
-                        setVisible(true);
-                        setConfirmLoading(false)
-                    }}
-                    autoComplete="off">
-                    <Form.Item label="Tài khoản"
-                               name="taiKhoan"
-                               rules={[
-                                   {
-                                       required: true,
-                                       message: 'Không được để trống',
-                                   },
-                               ]}>
-                        <Input type="text" placeholder="Tài khoản"/>
-                    </Form.Item>
+              Thoát
+            </button>
+            ,
+            <button
+              onClick={handleOk}
+              form="identity"
+              className="btn btn-green"
+              type="submit"
+            >
+              Đăng Nhập
+            </button>
+          </div>
+        }
+      >
+        <Form
+          name="identity"
+          labelCol={{ span: 24 }}
+          wrapperCol={{ span: 24 }}
+          onFinish={(values) => {
+            dispatch(fetchUserDetail(values));
+          }}
+          onFinishFailed={() => {
+            setVisible(true);
+            setConfirmLoading(false);
+          }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Tài khoản"
+            name="taiKhoan"
+            rules={[
+              {
+                required: true,
+                message: "Không được để trống",
+              },
+            ]}
+          >
+            <Input type="text" placeholder="Tài khoản" />
+          </Form.Item>
 
-                    <Form.Item label="Mật khẩu"
-                               name='matKhau'
-                               rules={[
-                                   {
-                                       required: true,
-                                       message: 'Không được để trống',
-                                   },
-                               ]}>
-                        <Input.Password type="password" placeholder="Mật khẩu"/>
-                    </Form.Item>
-                </Form>
-            </Modal>
+          <Form.Item
+            label="Mật khẩu"
+            name="matKhau"
+            rules={[
+              {
+                required: true,
+                message: "Không được để trống",
+              },
+            ]}
+          >
+            <Input.Password type="password" placeholder="Mật khẩu" />
+          </Form.Item>
+        </Form>
+      </Modal>
 
-            <h2 className=" bg-yellow-400 font-semibold font-sans text-white pl-32  text-3xl lg:leading-[200px]">
-                THÔNG TIN CÁ NHÂN
-            </h2>
-            <div
-                className=" md:w-5/6 md:min-height-[600px]  mx-auto  border-2  xl:h-screen lg:container lg:h-screen  shadow-xl ">
-                <Tabs type="card" className="tab_detail">
-                    <TabPane
-                        tab={<p className="lg:text-lg">Thông tin cá nhân</p>}
-                        key="1"
-                    >
-                        <Profile/>
-                    </TabPane>
-                    <TabPane
-                        className=""
-                        tab={<p className="lg:text-lg">Khoá học của tôi</p>}
-                        key="2"
-                    >
-                        <CourseList/>
-                    </TabPane>
-                </Tabs>
-            </div>
-        </div>
-    );
+      <h2 className=" bg-yellow-400 font-semibold font-sans text-white pl-32  text-3xl lg:leading-[200px]">
+        THÔNG TIN CÁ NHÂN
+      </h2>
+      <div className=" md:w-5/6 md:min-height-[600px]  mx-auto  border-2  xl:h-screen lg:container lg:h-screen  shadow-xl ">
+        <Tabs type="card" className="tab_detail">
+          <TabPane
+            tab={<p className="lg:text-lg">Thông tin cá nhân</p>}
+            key="1"
+          >
+            <Profile />
+          </TabPane>
+          <TabPane
+            className=""
+            tab={<p className="lg:text-lg">Khoá học của tôi</p>}
+            key="2"
+          >
+            <CourseList />
+          </TabPane>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
 
 export default Layout(UserDetail);
